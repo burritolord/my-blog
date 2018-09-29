89bab5a15666336bd0e19fe782a05f8c30bbd4c5
Nick James <thenickjames@gmail.com>
2018-09-28 17:52:03 -0700

    mysql working for production and development mode

--- ghost/wait-for-it.sh
+++ ghost/wait-for-it.sh
@@ -3,7 +3,7 @@ set -e
 host="$1"
 shift
 cmd="$@"
-until mysql -hmysql -p"3306" -u"${database__connection__user}" -p"${database__connection__password}" -D"${database__connection__database}"; do
+until mysql -hghost_db -p"3306" -u"${database__connection__user}" -p"${database__connection__password}" -D"${database__connection__database}"; do
   >&2 echo "MySQL is unavailable - sleeping"
   sleep 1
 done
